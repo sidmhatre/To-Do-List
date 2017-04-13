c0b62d684bf3bb61561688d6f422dac6a12c6ada
Siddhesh Mhatre <Sid@Siddheshs-MacBook-Pro.local>
2017-04-13 13:04:27 +0530

    Initial commit

---
+++ To Do List/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  To Do List
+//
+//  Created by Siddhesh Mhatre on 13/04/17.
+//  Copyright © 2017 Siddhesh Mhatre. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
